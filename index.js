d66dd36f6b221c433201de6244d0b3c2f8451398
Codersera <info@codersera.com>
2019-07-16 13:33:02 +0100

    changing the npm package name

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 #!/usr/bin/env node
 
 const execSync = require('child_process').execSync;
-const fs = require('fs');
-const path = require('path');
+const prompt = require('prompt');
+let gName = 'expo-starter-kit';
 
-execSync('git clone https://github.com/codersera-repo/expo-native-starter-kit.git', {stdio: 'inherit'});
+prompt.get([{
+    name: 'name',
+    description: 'Enter the project name, the directory will be created with the same name.',
+    type: 'string',
+    required: true
+}], function(err, results) {
+    console.log(results);
+    gName = results['name'];
+});
+
+execSync(`git clone https://github.com/codersera-repo/expo-native-starter-kit.git ${gName}`, {stdio: 'inherit'});
